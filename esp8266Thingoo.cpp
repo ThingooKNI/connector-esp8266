f5843f59f44bde365e0a2ece7b70d57fb694bd06
ninjavaz <d1n22@onet.pl>
2021-02-22 17:15:27 +0100

    First commit of new library for esp8266

---
+++ esp8266Thingoo.cpp
@@ -0,0 +1,9 @@
+/***************************************************************
+   esp8266Thingoo is a library for the ESP8266/Arduino platform
+   to communicate with Thingoo API
+   Built for KNI Project "Thingoo" https://github.com/ThingooKNI/
+ **************************************************************/
+
+#include "esp8266Thingoo.h"
+
+
---
+++ esp8266Thingoo.h
@@ -0,0 +1,33 @@
+/***************************************************************
+   esp8266Thingoo is a library for the ESP8266/Arduino platform
+   to communicate with Thingoo API
+   Built for KNI Project "Thingoo" https://github.com/ThingooKNI/
+ **************************************************************/
+
+#ifndef esp8266Thingoo_h
+#define esp8266Thingoo_h
+
+
+
+
+class esp8266Thingoo {
+
+  public:
+
+
+
+
+
+
+  private:
+
+
+};
+
+
+
+
+
+#endif
+
+
