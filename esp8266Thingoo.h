02db42924318a4986246f34f335cefb3ed12586f
ninjavaz <d1n22@onet.pl>
2021-03-16 21:26:28 +0100

    Fast change

--- esp8266Thingoo.h
+++ esp8266Thingoo.h
@@ -7,21 +7,44 @@
 #ifndef esp8266Thingoo_h
 #define esp8266Thingoo_h
 
+#include "Arduino.h"
+#include "WiFiClientSecure.h"
 
-
+#include <ArduinoJson.h>
 
 class esp8266Thingoo {
 
+
+    
+
   public:
+    
+    esp8266Thingoo();
+    void _send_token_request();
 
 
+    void client_credentials(String client_id, String secret_key);
+    void connect();
+    void _get_token();
+    
+    StaticJsonDocument<2048> doc;
+    WiFiClientSecure httpsClient; 
+    const char* fingerprint;
+    const char* HOST;
+    int httpsPort;  //HTTPS= 443 and HTTP = 80
 
+    String END_POINT;
+    
+
+    
+   
 
 
 
   private:
-
-
+    String _SECRET_KEY;
+    String _CLIENT_ID;
+    String _ACCESS_TOKEN;
 };
 
 
