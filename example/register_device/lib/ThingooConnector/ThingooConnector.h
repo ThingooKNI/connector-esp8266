6190d4d2a317efda8f667cc8907a0eda77a38fec
ninjavaz <d1n22@onet.pl>
2021-03-18 15:35:25 +0100

    http client works

--- example/register_device/lib/ThingooConnector/ThingooConnector.h
+++ example/register_device/lib/ThingooConnector/ThingooConnector.h
@@ -8,55 +8,37 @@
 #define ThingooConnector_h
 
 #include <Arduino.h>
-#include <WiFiClientSecure.h> 
+#include <WiFiClientSecure.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-#include <WiFiManager.h>
 #include <WiFiClientSecureBearSSL.h>
-
 
 class ThingooConnector {
 
+public:
+    ThingooConnector(const char* host);
 
-    
-
-  public:
-    
-    ThingooConnector();
-
-    
     void set_client_credentials(String client_id, String secret_key);
-    void set_fingerprint(uint8_t fingerprint);
+    void set_fingerprint(const char* fingerprint);
+    void api_request();
     void connect();
-    
+
+    HTTPClient http;
+
     DynamicJsonDocument doc();
-    WiFiClientSecure httpsClient; 
-    const char* fingerprint;
+    int https_port; //HTTPS=443
     const char* host;
-    int https_port;  //HTTPS= 443 and HTTP = 80
-
-    String token_endpoint;
-    
-
-    
-   
-
-
-
-  private:
-    void _send_token_request();
+    String register_endpoint;
+    String readings;
+    String devices;
     void _get_token();
-    
-    uint8_t _fingerprint;
+
+private:
+    const char* _host;
+    const char* _fingerprint;
     String _secret_key;
     String _client_id;
     String _access_token;
 };
 
-
-
-
-
 #endif
-
-
--- example/register_device/src/main.cpp
+++ example/register_device/src/main.cpp
@@ -1,59 +1,43 @@
-// #include "ThingooConnector.h"
+#include "ThingooConnector.h"
 
 #include <Arduino.h>
-#include <WiFiClientSecure.h> 
+#include <WiFiClientSecure.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 #include <WiFiManager.h>
 #include <WiFiClientSecureBearSSL.h>
 
-
-
-
 //SHA1 finger print of certificate
-const uint8_t fingerprint[20] = {0xF6, 0x23, 0x3E, 0xAC, 0x7A, 0x1D, 0x03, 0x63, 0x15, 0xE2, 0x4F, 0x57, 0xB6, 0x10, 0x23, 0x2E, 0x22, 0x53, 0x51, 0x4E};
+const char* fingerprint = "95 E1 00 D0 43 BC CE 7E 24 67 71 BC 43 77 15 23 78 74 FF C5";
 
-
+const char* host = "dev.thingoo.xyz";
 const char* SECRET_KEY = "101ed902-3a95-48b1-b8c6-9a4648151050";
-
 
 //=========================================================
 //                    Setup                               =
 //=========================================================
 
+ThingooConnector connector(host);
 
-
-// ThingooConnector connector;
-
-
-void setup() {
-    Serial.begin(115200);
-
-  
+void setup()
+{
     Serial.begin(115200);
     Serial.println("Starting WiFi");
- 
     WiFiManager wifiManager;
     wifiManager.autoConnect("ThingooDevice");
 
-    
     // connector.set_client_credentials("thingoo-device", SECRET_KEY);
     // connector.set_fingerprint(fingerprint);
     // connector.connect();
 
-    
-
-
-
+    connector.set_client_credentials("thingoo-device", "101ed902-3a95-48b1-b8c6-9a4648151050");
+    connector.set_fingerprint(fingerprint);
+    connector._get_token();
 }
-
-
-
 
 //=======================================================================
 //                    Main Loop
 //=======================================================================
-void loop() {
-  
+void loop()
+{
 }
-//=======================================================================
